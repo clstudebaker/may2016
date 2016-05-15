73ff1d04dd804f6b975b9ecc4e4444687c8464e5
clstudebaker <clstudebaker@gmail.com>
2016-05-15 12:34:04 -0700

    Add files via upload

---
+++ basicjs.js
@@ -0,0 +1,11 @@
+
+function SimpleController($scope){
+
+    $scope.customers= [
+{name:'Dave Jones', city:'Phoenix'},
+{name:'Jamie Riley', city: 'Atlanta'},
+{name:'Heedy Wahlin',city:'Chandler'},
+{name:'Thomas Winter',city:'Seattle'}
+];
+}
+
